b4fc62cc21f9da1e4f3f601d6a1a6da18a387320
Christia T <christiatidwell@bellsouth.net>
2018-04-20 01:49:04 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,11 +1,11 @@
-def oxford_comma(array, new_string)
+def oxford_comma(array)
   if array.size == 1
     array.join
   elsif array.size == 2
     array.join(" and ")
   elsif array.size >= 3
-     array.join(",") = new_string
-     new_string<<" and "
+     array.join(",") 
+         <<" and "
   else
   end
 end
