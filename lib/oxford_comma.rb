2a59401c9e46d20b819bc3379b4977c1f6c9f741
Christia T <christiatidwell@bellsouth.net>
2018-04-20 01:13:26 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,9 +1,8 @@
 def oxford_comma(array)
-  array.join(",") = new_array
 
   if array.size == 2
     array.join("and")
   elsif array.size == 3
-    array.join("")
-  elsif array.size ==
+    array.join(",")
+  elsif array.size 
 end
