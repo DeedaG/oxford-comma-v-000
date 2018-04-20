69f69411dcbf78e5f474d931f80bbef2a9687b97
Christia T <christiatidwell@bellsouth.net>
2018-04-20 02:24:53 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,7 +4,7 @@ def oxford_comma(array)
   elsif array.size == 2
     array.join(" and ")
   elsif array.size >= 3
-     array.join(",") except [-2]
+     array.join(",") except
      array.insert(-2, " and ")
 
   else
